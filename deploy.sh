ec21af32e59856e48016dfee660f19aaf4e30961
Matthew A Snell <Astro7467@XLTech.io>
2019-10-16 09:09:11 +0800

    -n

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,33 @@ set -e
 
 cd ~/git/www-xltech-static
 
+PUBLISH="NO"
+
 case $1 in
   -h|-\?|--help)                  # Call a "show_help" function to display a synopsis, then exit.
     ##show_help
     exit
     ;;
-  -p|--publish|-y|--yes*)
+  -p|--publish|-y|--yes|-l|--live)
+    PUBLISH="YES"
+    shift
+    break
+    ;;
+  -np|--nopublish|--no-publish|-d|--draft|--no|-t|--test)
+    PUBLISH="NO"
+    shift
+    break
+    ;;
+esac
+
+case $PUBLISH in
+  YES)
     echo -e "\t *** Publishing to Live Website\n"
     if [ -e no.publish ]; then
       rm no.publish
     fi
     touch yes.publish
-    shift
     break
-    ;;
-  -np|--nopublish|--no-publish|-d|--draft|--no|-t|--test)
-    shift
     ;;
   *)                              # Default case: If no more options then break out of the loop.
     echo -e "\t *** NOT Publishing to Live Website - Test/Draft Mode \n"
@@ -30,14 +41,6 @@ case $1 in
     touch no.publish
     break
 esac
-
-if [ ! -n "$1" ]; then
-    echo -e "\t *** NOT Publishing to Live Website - Test/Draft Mode \n"
-    if [ -e yes.publish ]; then
-      rm yes.publish
-    fi
-    touch no.publish
-fi
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
